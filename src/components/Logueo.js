def72ab9b4f0d9a8641cb6b5585c7bd64800c2d3
unknown <bryan_narutito@hotmail.com>
2022-11-10 16:16:40 -0600

    10112022

--- src/components/Logueo.js
+++ src/components/Logueo.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Stack, Container, Form, Button } from "react-bootstrap";
+import { Stack, Container, Form, Button, Alert } from "react-bootstrap";
 
 import firebaseApp from "../credenciales";
 import {
@@ -8,35 +8,59 @@ import {
   signInWithEmailAndPassword,
   signInWithRedirect,
   GoogleAuthProvider,
+  FirebaseAuthException
 } from "firebase/auth";
 const auth = getAuth(firebaseApp);
 const googleProvider = new GoogleAuthProvider();
 
 const Logueo = () => {
+  const [Error, setError] = useState('');
   const [estaRegistrandose, setEstaRegistrandose] = useState(false);
 
   async function submitHandler(e) {
     e.preventDefault();
+    setError('')
     const correo = e.target.formBasicEmail.value;
     const contra = e.target.formBasicPassword.value;
 
-    if (estaRegistrandose) {
-      //si se registra
+  if (estaRegistrandose) {
+    //si se registra
+    try {
       const usuario = await createUserWithEmailAndPassword(
         auth,
         correo,
         contra
       );
-    } else {
-      // si está iniciando sesión
-      signInWithEmailAndPassword(auth, correo, contra);
+    } catch (error) {
+      setError(error.message)
+      console.log(error.message)
     }
+   
+  } else {
+    // si está iniciando sesión
+    try {
+    await  signInWithEmailAndPassword(auth, correo, contra);
+    } catch (error) {
+      setError(error.message)
+      console.log(error.message)
+    }
+  
+    }
+      
+   
+
   }
+
+
+    
+  
 
   return (
     <Container>
-      <Stack gap={3}>
+      <Stack gap={3}> 
+      {Error&&<Alert variant="danger">{Error}</Alert>}
         <h1>{estaRegistrandose ? "Regístrate" : "inicia sesión"}</h1>
+        
         <Form onSubmit={submitHandler}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
